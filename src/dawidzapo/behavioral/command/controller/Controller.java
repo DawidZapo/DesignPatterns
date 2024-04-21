eecb38a93f3ff4d6cc3b494c12680145c3fbae58
DawidZapo <dawidzapo@gmail.com>
2024-04-21 22:20:36 +0200

    Behavioral: Command design, history added (Stack)

--- src/dawidzapo/behavioral/command/controller/Controller.java
+++ src/dawidzapo/behavioral/command/controller/Controller.java
@@ -2,15 +2,26 @@ package dawidzapo.behavioral.command.controller;
 
 import dawidzapo.behavioral.command.command_interface.Command;
 
+import java.util.Stack;
+
 public class Controller implements Command {
     private Command command;
+    private Stack<Command> history;
 
     public void setCommand(Command command){
         this.command = command;
+        this.history = new Stack<>();
     }
 
     @Override
     public void execute() {
         command.execute();
+        push(command);
+    }
+    public void push(Command command){
+        history.push(command);
+    }
+    public Command pop(){
+        return history.pop();
     }
 }
